288b09c4b7e3174ca19167f1485820679b063182
Swapndar <swapnil.darshanale@amdocs.com>
2024-10-02 18:34:31 +0530

    error handel

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import "./sidebar.css";
+import "./Sidebar.css";
 import { assets } from "../../assets/assets";
 import { Context } from "../../context/Context";
 const Sidebar = () => {
